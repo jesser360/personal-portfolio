312eab1368f03b37d38d8ca2340e2b65c81c0930
Jesser360 <jesser360@gmail.com>
2017-05-15 21:44:31 -0700

    added portfolio section, navbar, backround image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,40 @@
 // sanity check
 console.log("app.js linked.");
+
+$(document).ready(function() {
+
+$('.home-pic').mouseenter(function(){
+  $('#msg').removeClass('hidden');
+})
+$('.home-pic').mouseleave(function(){
+  $('#msg').addClass('hidden');
+})
+
+$('.item').click(function(){
+$(this).siblings('.active').removeClass('active');
+$(this).addClass('active');
+
+if ($(this).hasClass("ruby")){
+  $('#ruby-proj').removeClass('hidden');
+  $('#free-proj').addClass("hidden");
+  $('#games-proj').addClass("hidden");
+  $('#react-proj').addClass("hidden");
+} else if ($(this).hasClass("free")){
+  $('#free-proj').removeClass('hidden');
+  $('#ruby-proj').addClass("hidden");
+  $('#games-proj').addClass("hidden");
+  $('#react-proj').addClass("hidden");
+} else if($(this).hasClass("games")){
+  $('#games-proj').removeClass('hidden');
+  $('#free-proj').addClass("hidden");
+  $('#ruby-proj').addClass("hidden");
+  $('#react-proj').addClass("hidden");
+} else if ($(this).hasClass("react")) {
+  $('#react-proj').removeClass('hidden');
+  $('#free-proj').addClass("hidden");
+  $('#ruby-proj').addClass("hidden");
+  $('#games-proj').addClass("hidden");
+}
+});
+
+});
